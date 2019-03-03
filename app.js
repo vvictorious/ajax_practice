4daf8f797205dc106848f64ea18317871f06bc30
vvictorious <vvictorious30@gmail.com>
2019-03-03 10:56:32 -0700

    dynamically getting to different endpoints on each click

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+let pageCounter = 1;
 const button = document.getElementById('btn');
 const div = document.getElementById('animal-info');
+
 btn.addEventListener('click', (e) => {
 	let ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter +'.json');
 	ourRequest.onload = function(){
 		const data = JSON.parse(ourRequest.responseText);
 		for(let i = 0; i < data.length; i += 1){
 			const li = document.createElement('li');
-			li.textContent= data[i];
+			li.textContent= data[i].name + ' is a ' + data[i].species;
 			div.appendChild(li);
 		}
 	}
 	ourRequest.send();
+	pageCounter++;
 });
 
 
