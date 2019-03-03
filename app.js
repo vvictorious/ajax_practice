4bff50f5e566d5db23f49509d6a8660aa59a526b
vvictorious <vvictorious30@gmail.com>
2019-03-03 10:37:55 -0700

    i ajaxed to my endpoint

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log ('hi');
+
+
+let ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function(){
+	const data = JSON.parse(ourRequest.responseText);
+	console.log(data);
+}
+ourRequest.send();
