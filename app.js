51ef86ad6186ef5c2a344a2e0ec83bc43e820c22
vvictorious <vvictorious30@gmail.com>
2019-03-03 10:46:26 -0700

    appending objects from api to the dom

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+const button = document.getElementById('btn');
+const div = document.getElementById('animal-info');
+btn.addEventListener('click', (e) => {
+	let ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.onload = function(){
+		const data = JSON.parse(ourRequest.responseText);
+		for(let i = 0; i < data.length; i += 1){
+			const li = document.createElement('li');
+			li.textContent= data[i];
+			div.appendChild(li);
+		}
+	}
+	ourRequest.send();
+});
 
 
-let ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function(){
-	const data = JSON.parse(ourRequest.responseText);
-	console.log(data);
-}
-ourRequest.send();
+
